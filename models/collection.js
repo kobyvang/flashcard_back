51c783470a8977be7fde82839293e60892a91525
kobyvang <80074281+kobyvang@users.noreply.github.com>
2021-07-07 05:36:42 -0500

    Add files via upload

--- models/collection.js
+++ models/collection.js
@@ -1,1 +1,21 @@
+const mongoose = require('mongoose');
 
+
+
+
+
+const productSchema = new mongoose.Schema({
+    name: { type: String, required: true, minlength: 2, maxlength: 255 },
+    description: { type: String, required: true },
+    dateModified: { type: Date, default: Date.now },
+   });
+   
+
+   const Product = mongoose.model('Product', productSchema);
+
+   
+
+
+
+
+   module.exports = Product;
